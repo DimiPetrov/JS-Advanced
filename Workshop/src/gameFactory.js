80305e7a5e0c6701cc5e3913653793be535e1aa6
DimiPetrov <d_petrov@yahoo.com>
2021-11-11 14:45:59 +0200

    wizard game

--- Workshop/src/gameFactory.js
+++ Workshop/src/gameFactory.js
@@ -1,9 +1,9 @@
 function gameFactory() {
-    let getState = gameStateFactory();
-    let { wizard } = getState();
+    let { wizard, bugStats, fireballStats } = state;
 
     let startScreen = document.querySelector('.start-screen');
     let playScreen = document.querySelector('.play-screen');
+    let scoreScreen = document.querySelector('.score-screen');
     let wizardElement = createWizard(wizard.x, wizard.y);
 
     playScreen.appendChild(wizardElement);
@@ -11,7 +11,32 @@ function gameFactory() {
     let factory = {
         startScreen,
         playScreen,
-        wizardElement
+        scoreScreen,
+        wizardElement,
+        createBug: () => {
+            let bugElement = document.createElement('div');
+        
+            bugElement.classList.add('bug');
+            bugElement.style.width = bugStats.width + 'px';
+            bugElement.style.height = bugStats.height + 'px';
+
+            bugElement.style.left = playScreen.offsetWidth - bugStats.width + 'px';
+            bugElement.style.top = (playScreen.offsetHeight - bugStats.height) * Math.random() + 'px';
+
+            playScreen.appendChild(bugElement);
+        },
+        createFireball: () => {
+            let fireballElement = document.createElement('div');
+
+            fireballElement.classList.add('fireball');
+            fireballElement.style.width = fireballStats.width + 'px';
+            fireballElement.style.height = fireballStats.height + 'px';
+
+            fireballElement.style.left = wizard.x + 'px';
+            fireballElement.style.top = wizard.y + 'px';
+
+            playScreen.appendChild(fireballElement);
+        }
     };
 
     return factory;
--- Workshop/src/gameState.js
+++ Workshop/src/gameState.js
@@ -1,15 +1,44 @@
-function gameStateFactory() {
-    let state = {
-        gameOver: false,
-        wizard: {
-            x: 100,
-            y: 200,
-            speed: 5
-        },
-        keys: {}
-    };
+var state = {
+    gameOver: false,
+    score: 0,
+    wizard: {
+        x: 100,
+        y: 200,
+        width: 82,
+        height: 100,
+        speed: 7,
+        gravity: 2,
+    },
+    keys: {},
+    bugStats: {
+        nextBugCreation: 0,
+        maxCreationInterval: 1500,
+        speed: 3,
+        width: 50,
+        height: 50,
+        score: 500,
+    },
+    fireballStats: {
+        width: 40,
+        height: 40,
+        speed: 10,
+        nextFireballCreation: 0,
+        attackSpeed: 400,
+    }
+};
 
-    return function() {
-        return state;
-    };
-}
+// function gameStateFactory() {
+//     let state = {
+//         gameOver: false,
+//         wizard: {
+//             x: 100,
+//             y: 200,
+//             speed: 5
+//         },
+//         keys: {}
+//     };
+
+//     return function() {
+//         return state;
+//     };
+// }
